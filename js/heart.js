99dee90e272c17c16c3f5eccb03bedc7c235f86c
guslozano-dev <ing.gus.lozano@gmail.com>
2026-07-30 08:03:08 -0500

    Delete js directory

--- js/heart.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const wishlistBtns = document.querySelectorAll('.btn-wishlist');
-    wishlistBtns.forEach(btn => {
-      btn.addEventListener('click', function(event) {
-        event.preventDefault(); // evita que el enlace navegue
-        btn.classList.toggle('active');
-      });
-    });
-  });
